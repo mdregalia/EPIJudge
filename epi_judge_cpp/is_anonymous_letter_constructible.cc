ca3ddfbc7eb6c423ee4c3cdd5e3c0a5c28dea52d
meregali <meregali@microsoft.com>
2018-10-09 08:47:02 -0700

    Is_anonymous_letter_constructible

--- epi_judge_cpp/is_anonymous_letter_constructible.cc
+++ epi_judge_cpp/is_anonymous_letter_constructible.cc
@@ -1,9 +1,39 @@
 #include <string>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 using std::string;
+using namespace std;
 
 bool IsLetterConstructibleFromMagazine(const string& letter_text,
                                        const string& magazine_text) {
+	unordered_map<char, int> letterCount;
+
+	for (int i = 0; i < magazine_text.length(); i++)
+	{
+		if (!letterCount.count(magazine_text[i]))
+		{
+			letterCount[magazine_text[i]] = 1;
+		}
+		else
+		{
+			letterCount[magazine_text[i]]++;
+		}
+	}
+
+	for (int i = 0; i < letter_text.length(); i++)
+	{
+		if (!letterCount.count(letter_text[i]))
+		{
+			return false;
+		}
+
+		letterCount[letter_text[i]]--;
+		if (letterCount[letter_text[i]] < 0)
+		{
+			return false;
+		}
+	}
+
   // TODO - you fill in here.
   return true;
 }
