d7b1eea8dd2d1cc699d6a196ab7ec46365b70193
mdregalia <melissa.d.regalia@gmail.com>
2018-10-15 22:29:27 -0700

    three_sum

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -1,10 +1,93 @@
 #include <vector>
+#include <iostream>
 #include "test_framework/generic_test.h"
 using std::vector;
+using namespace std;
+
+void prntArray(vector<int> A)
+{
+	cout << "\nSorted: ";
+	for (int i = 0; i < A.size(); i++)
+	{
+		cout << A[i] << " ";
+	}
+	cout << "\n";
+}
+
+void swap(vector<int>& A, int a, int b)
+{
+	//cout << "a: "<< a << " b: " << b << "\n";
+	int tmp = A[a];
+	A[a] = A[b];
+	A[b] = tmp;
+}
+
+int partition(vector<int>& A, int start, int end)
+{
+	int pivot = A[end - 1];
+	int i = start;
+
+	while (start < end - 1)
+	{
+		if (A[start] <= pivot)
+		{
+			swap(A, i, start);
+			i++;
+			start++;
+		}
+		else
+		{
+			start++;
+		}
+	}
+	swap(A, i, end - 1);
+	return i;
+}
+
+void quicksort(vector<int>& A, int start, int end)
+{
+	if (start < end)
+	{
+		int p = partition(A, start, end);
+		quicksort(A, start, p);
+		quicksort(A, p + 1, end);
+	}
+}
 
 bool HasThreeSum(vector<int> A, int t) {
   // TODO - you fill in here.
-  return true;
+	//prntArray(A);
+
+	quicksort(A, 0, A.size());
+
+	//prntArray(A);
+
+	for (int i = 0; i < A.size(); i++)
+	{
+		int j = i;
+		int k = A.size() - 1;
+
+		while (j <= k)
+		{
+			//cout << i << " " << j << " " << k << "\n";
+			//cout << A[i] << " " << A[j] << " " << A[k] << "\n";
+
+			if (A[j] + A[k] < t - A[i])
+			{
+				j++;
+			}
+			else if (A[j] + A[k] > t - A[i])
+			{
+				k--;
+			}
+			else
+			{
+				return true;
+			}
+		}
+	}
+
+  return false;
 }
 
 int main(int argc, char* argv[]) {
